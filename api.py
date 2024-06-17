5e556e04545a854106e67beed260123182a9a680
bunnycou <noahjcou@gmail.com>
2024-06-17 11:42:22 -0400

    error prints

--- api.py
+++ api.py
@@ -36,6 +36,7 @@ def matches(region, puuid, gameType, queue):
 
             return matchlist[:startOfSession+1]
     else:
+        print("matches - " + req.status_code)
         return "err"
 
 def match(region, id):
@@ -43,6 +44,7 @@ def match(region, id):
     if (req.ok):
         return req.json()
     else:
+        print("match - " + req.status_code)
         return "err"
 
 def getpuuid(user, tag):
@@ -50,6 +52,7 @@ def getpuuid(user, tag):
     if (req.ok):
         return req.json()["puuid"]
     else:
+        print("puuid - " + req.status_code)
         return "err"
     
 def getWLKD(region, user, tag, gameType):
